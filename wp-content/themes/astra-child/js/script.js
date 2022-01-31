037e3151c9fd5a8e10470fcfae69d2502b294df1
Gouenji10 <excelblade10@gmail.com>
2022-01-31 13:18:38 +0545

    changes

--- wp-content/themes/astra-child/js/script.js
+++ wp-content/themes/astra-child/js/script.js
@@ -1,7 +1,20 @@
 jQuery(document).ready(function ($) {
-    
-    $('.woocommerce-checkout').find('#tgpc_enable_checkout_gift_wrapper').on('change',function(){
-        console.log('abac')
+
+    if($('.woocommerce-checkout').find('#tgpc_enable_checkout_gift_wrapper').prop('checked') == true)
+    {
+        $('p.gift-message').show();
+    }
+    else{
+        $('p.gift-message').hide();
+    }
+
+    $('.woocommerce-checkout').find('#tgpc_enable_checkout_gift_wrapper').on('change', function () {
+        if ($(this).prop("checked") == true) {
+            $('p.gift-message').show();
+        }
+        else if ($(this).prop("checked") == false) {
+            $('p.gift-message').hide();
+        }
     })
 
 
