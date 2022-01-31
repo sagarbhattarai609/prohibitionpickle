51e8cebdeb1a7262d2a343c6b9b87f2b2413f362
Gouenji10 <excelblade10@gmail.com>
2022-01-31 13:05:08 +0545

    custom field

--- wp-content/themes/astra-child/functions.php
+++ wp-content/themes/astra-child/functions.php
@@ -25,3 +25,25 @@ function child_enqueue_styles() {
 }
 
 add_action( 'wp_enqueue_scripts', 'child_enqueue_styles', 15 );
+
+
+/**
+
+* Add the field to the checkout page
+
+*/
+
+add_action( 'woocommerce_after_order_notes', 'tl_some_custom_checkout_field' );
+
+function tl_some_custom_checkout_field( $checkout ) 
+{
+	echo '<div id="some_custom_checkout_field"><h2>' . __('Heading') . '</h2>';
+	woocommerce_form_field( 'some_field_name', array(
+		'type'         => 'text',
+		'class'         => array('my-field-class form-row-wide'),
+		'label'         => __('Additional Field'),
+		'placeholder'   => __('Some hint'),
+		'required'     => true,
+	), $checkout->get_value( 'some_field_name' ));
+	echo '</div>';
+}
