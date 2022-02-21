19723a1b4802c418e26ce3660d0f9f8090c508e5
Gouenji10 <excelblade10@gmail.com>
2022-02-21 10:24:41 +0545

    changes

--- wp-content/themes/astra-child/functions.php
+++ wp-content/themes/astra-child/functions.php
@@ -18,9 +18,12 @@ define( 'CHILD_THEME_ASTRA_CHILD_VERSION', '1.0.0' );
  */
 function child_enqueue_styles() {
 
+	
+	wp_enqueue_style( 'magnific-popup-css', "https://cdnjs.cloudflare.com/ajax/libs/magnific-popup.js/1.1.0/magnific-popup.min.css");
 	wp_enqueue_style( 'custom-css', get_stylesheet_directory_uri() . '/css/custom.css' );
 	wp_enqueue_style( 'astra-child-theme-css', get_stylesheet_directory_uri() . '/style.css', array('astra-theme-css'), CHILD_THEME_ASTRA_CHILD_VERSION, 'all' );
 	
+	wp_enqueue_script( 'magnific-popup-js','https://cdnjs.cloudflare.com/ajax/libs/magnific-popup.js/1.1.0/jquery.magnific-popup.min.js',array('jquery'));
 	wp_enqueue_script( 'custom-script', get_stylesheet_directory_uri() . '/js/script.js', array( 'jquery' ) );
 }
 
