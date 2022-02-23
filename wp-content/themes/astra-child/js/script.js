78212daedafcb02eafd4b044eb608cef1790fc01
Gouenji10 <excelblade10@gmail.com>
2022-02-23 16:23:47 +0545

    changes

--- wp-content/themes/astra-child/js/script.js
+++ wp-content/themes/astra-child/js/script.js
@@ -27,10 +27,33 @@ jQuery(document).ready(function ($) {
           });
     });
 
-
+    /**
+     * 
+     * Cart Page
+     * 
+    */
     if ($('body.woocommerce-cart').find('div.so-offer-content.so-inline').length !== 0) {
-        $('div.so-offer-content.so-inline').eq(0).before('<div class="so-offer-top-heading"><div class="text">Frequently Bought Together</div><div class="skip_link mobile_sticky"><a href="#" id="goToCheckout">Skip & Continue to Checkout <span class="ri ri-chevron-down"></span></a></div></div>')
+        $('div.so-offer-content.so-inline').eq(0).before('<div class="so-offer-top-heading"><div class="text">Frequently Bought Together</div><div class="skip_link mobile_sticky"><a href="#" id="goToCouponCart">Skip & Continue to Checkout <span class="ri ri-chevron-down"></span></a></div></div>')
     }
+    //  // if cartpage
+    //  if (jQuery('body.page-id-20242').find('div.so-offer-content.so-inline').length !== 0) {
+    //     jQuery('div.so-offer-content.so-inline').eq(0).before('<div class="so-offer-top-heading"><div class="text">Frequently Bought Together</div><div class="skip_link mobile_sticky"><a href="/checkout" id="goToCouponCart">Skip & Continue to Cart <span class="ri ri-chevron-down"></span></a></div></div>')
+    //     jQuery('div.so-offer-content.so-inline').eq(-1).find('.entry-content').css('margin-bottom', '50px')
+    // }
+
+
+    jQuery(document).on('click', '#goToCouponCart', function () {
+        jQuery('html,body').animate({
+            scrollTop: jQuery("form.woocommerce-cart-form").offset().top -= 100
+        }, 1000)
+        return false;
+    })
+    jQuery(document).on('click', '#goToCheckout', function () {
+        jQuery('html,body').animate({
+            scrollTop: jQuery("form.woocommerce-checkout").offset().top -= 100
+        }, 1000)
+        return false;
+    })
 
     // // if cartpage
     // if (jQuery('body.page-id-20242').find('div.so-offer-content.so-inline').length !== 0) {
