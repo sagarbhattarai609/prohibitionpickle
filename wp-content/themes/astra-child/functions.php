a8fa6e3c4932ff5805289a5397f88ebb770f2ba7
Gouenji10 <excelblade10@gmail.com>
2022-03-04 16:17:28 +0545

    changes

--- wp-content/themes/astra-child/functions.php
+++ wp-content/themes/astra-child/functions.php
@@ -139,9 +139,7 @@ function change_shipping_text($text) {
 function product_note_above_short_desc_func(){
 
 	global $product;
-	$size = array_shift( wc_get_product_terms( $product->id, 'pa_product-note', array( 'fields' => 'names' ) ) );
-	print_r($size);
-	
-    echo 'TACOSs!';
+	$size = array_shift( wc_get_product_terms( $product->id, 'pa_product-note', array( 'fields' => 'names' ) ) );	
+    echo $size;
 }
 add_action( 'woocommerce_single_product_summary', 'product_note_above_short_desc_func', 15 );
