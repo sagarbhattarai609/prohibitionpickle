88552008153b3c07352d5e15881149c51a64876b
Gouenji10 <excelblade10@gmail.com>
2022-03-06 15:45:54 +0545

    changes

--- wp-content/themes/astra-child/admin/js/admin-script.js
+++ wp-content/themes/astra-child/admin/js/admin-script.js
@@ -1,5 +1,5 @@
-jQuery(document).ready(function($){
-    $('#minimum_amt_free_shipping_charge','#shipping_charge','#minimum_order_limit').on('change',function(){
+jQuery(document).ready(function ($) {
+    $('#minimum_amt_free_shipping_charge , #shipping_charge , #minimum_order_limit').on('change', function () {
         $('.wc-shipping-zone-method-save').removeAttr('disabled')
     })
 })
